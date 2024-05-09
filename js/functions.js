229877ce7662e40e3367fc16fa46cc672b43fe2f
Dihep <dihep.demayo@gmail.com>
2024-05-09 21:20:56 +0200

    M2- Stampa in pagina.

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,28 @@
-//Funzione di stampa in console
+/*//Funzione di stampa in console
 function stampaConsole(arrayOggetti) {
     arrayOggetti.forEach(teamMember => {
         console.log(teamMember.nome, teamMember.ruolo, teamMember.immagine);
+    });
+};*/
+
+//Funzione di stampa in pagina
+function stampaInPagina(arrayOggetti) {
+
+    //Obbiettivo di stampa
+    const target = document.querySelector("main");
+
+    //Stampa in ciclo
+    arrayOggetti.forEach(teamMember => {
+        //Contenitore output
+        let outDiv = document.createElement("div");
+        outDiv.classList.add("teamMember");
+        //Stampa delle proprietà
+        for (const key in teamMember) {
+            let outSpan = document.createElement("span");
+            outSpan.classList.add("attribute")
+            outSpan.innerText = `${key}: ${teamMember[key]}`;
+            outDiv.append(outSpan);
+        }
+        target.append(outDiv);
     });
 };
--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // Creazione variabili
 const team = [
-    {nome:`Wayne Barnett`, ruolo:`Founder & CEO`, immagine:`wayne-barnett-founder-ceo.jpg`},
-    {nome:`Angela Caroll`, ruolo:`Chief Editor`, immagine:`angela-caroll-chief-editor.jpg`},
-    {nome:`Walter Gordon`, ruolo:`Office Manager`, immagine:`walter-gordon-office-manager.jpg`},
-    {nome:`Angela Lopez`, ruolo:`Social Media Manager`, immagine:`angela-lopez-social-media-manager.jpg`},
-    {nome:`Scott Estrada`, ruolo:`Developer`, immagine:`scott-estrada-developer.jpg`},
-    {nome:`Barbara Ramos`, ruolo:`Graphic Designer`, immagine:`barbara-ramos-graphic-designer.jpg`}
+    {Nome:`Wayne Barnett`, Ruolo:`Founder & CEO`, Immagine:`wayne-barnett-founder-ceo.jpg`},
+    {Nome:`Angela Caroll`, Ruolo:`Chief Editor`, Immagine:`angela-caroll-chief-editor.jpg`},
+    {Nome:`Walter Gordon`, Ruolo:`Office Manager`, Immagine:`walter-gordon-office-manager.jpg`},
+    {Nome:`Angela Lopez`, Ruolo:`Social Media Manager`, Immagine:`angela-lopez-social-media-manager.jpg`},
+    {Nome:`Scott Estrada`, Ruolo:`Developer`, Immagine:`scott-estrada-developer.jpg`},
+    {Nome:`Barbara Ramos`, Ruolo:`Graphic Designer`, Immagine:`barbara-ramos-graphic-designer.jpg`}
 ]
 
-stampaConsole(team);
+stampaInPagina(team);
